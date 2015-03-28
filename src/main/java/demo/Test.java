c40ef4b8a2a96bafee13c47f03ba65dfd265e139
mingfei <mingfei.net@gmail.com>
2015-03-28 11:52:43 +0800

    create test class
    att;

---
+++ src/main/java/demo/Test.java
@@ -0,0 +1,10 @@
+package demo;
+
+/**
+ * Created by Administrator on 2015/3/28.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello".toUpperCase());
+    }
+}
