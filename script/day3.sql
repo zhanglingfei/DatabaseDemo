54fed531f25674d518813a5b28d86fcc715d59fb
mingfei <mingfei.net@gmail.com>
2015-03-28 10:49:31 +0800

    update

--- script/day3.sql
+++ script/day3.sql
@@ -22,3 +22,24 @@ where e.deptno = d.deptno and d.dname = 'SALES'
 ;
 
 --github for windows...
+
+SELECT max(hiredate)
+FROM emp
+GROUP BY deptno
+;
+
+CREATE TABLE emp_test
+  AS
+  SELECT ename, empno
+  FROM emp
+  ;
+
+SELECT *
+FROM emp_test;
+
+DELETE FROM emp_test;
+
+INSERT INTO emp_test
+    SELECT ename, empno FROM emp
+WHERE deptno = 10
+;
