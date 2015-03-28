130bef60e126fd8214bbe7b66ea65a35be98d1cf
mingfei <mingfei.net@gmail.com>
2015-03-28 10:29:53 +0800

    edit day3.sql

--- script/day3.sql
+++ script/day3.sql
@@ -1,1 +1,24 @@
 SELECT * FROM cat;
+
+SELECT * FROM dept;
+
+SELECT ename, empno, deptno
+FROM emp
+WHERE deptno = ( -->, < >=, <= IN NOT IN
+
+  SELECT deptno
+  FROM dept
+  WHERE dname = 'SALES'--;?
+);
+
+SELECT deptno
+FROM dept
+WHERE dname = 'SALES';
+
+
+SELECT e.ename, e.empno, d.deptno
+FROM emp e, dept d
+where e.deptno = d.deptno and d.dname = 'SALES'
+;
+
+--github for windows...
