96191775c910ad138fe4078d098ba4849293d9a3
mingfei <mingfei.net@gmail.com>
2015-03-28 13:36:08 +0800

    edit Test class
    att;

--- src/main/java/demo/Test.java
+++ src/main/java/demo/Test.java
@@ -1,10 +1,17 @@
 package demo;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Administrator on 2015/3/28.
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello".toUpperCase());
+
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
     }
+
+
 }
